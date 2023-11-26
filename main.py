7103c7143bbc3811e9d7a6ba936c99e4672a4d16
tima-kuchin <kuchin.timur@bk.ru>
2023-11-26 21:56:39 +0700

    Add files

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ app = FastAPI()
 
 @app.get('/')
 async def f_index():
-    return 'Кучин Тимур Анатольевич'
+    return {"FIO": "Кучин Тимур Анатольевич"}
 
 
 @app.get('/tools')
 async def f_indexT():
-    return '+7-961-240-22-66'
+    return {"Phone": "+7-961-240-22-66"}
 
 
 @app.get('/users')
 async def f_indexU():
-    return 'C#, Python, Git, PostgreSQL'
+    return {"Skills": "C#, Python, Git, PostgreSQL"}
